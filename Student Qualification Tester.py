6935bfd78c1fe4ae964e36fb86c4b968f943d032
Jackson Duchow <jduchow@ivytech.edu>
2026-01-23 06:28:42 -0500

    actaully done with testing

--- Student Qualification Tester.py
+++ Student Qualification Tester.py
@@ -6,11 +6,12 @@ This app request a GPA for a student and tests if they're are eligable for the H
 print("Welcome to the Student Qualification Tester!\
    \nThis app will determine if a student qualifies for the Honor Roll or Dean's List based on their GPA.")
 last_name = input("Please enter the student's last name: ")
+gpa = 0.0
 while last_name != 'ZZZ':
-   if gpa in range(0.0, 4.1):
+   if gpa >= 0.0 and gpa <= 4.0:
       first_name = input("Please enter the student's first name: ")
    gpa = float(input("Please enter the student's GPA (0.0 - 4.0): "))
-   if gpa in range(0.0, 4.1):
+   if gpa < 0.0 or gpa > 4.0:
       print("Invalid GPA entered. Please enter a GPA between 0.0 and 4.0.")
    else:
       if gpa >= 3.5:
