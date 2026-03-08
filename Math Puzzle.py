e2fcc2b17362f4b67c575df8fa10deb670c9df64
Your Name <you@example.com>
2026-03-08 09:28:16 -0400

    up to step 4 done

--- Math Puzzle.py
+++ Math Puzzle.py
@@ -8,14 +8,17 @@ This program finds any number that can be multpied by 4 and be reversed ie:2178*
 
 
 
-def reverseFinder(x):
+def reverseVerifier(x):
+    #turns x into a string and reverses it
     rev = str(x)[::-1]
-    prod = x*4
-    if str(prod) == rev:
-        return True
-    else: 
-        return False
-    
-for i in range(1000,10000):
-    if reverseFinder(i):
-        print(i)
+    #string of the product of x and 4
+    prod = str(x*4)
+    #returns the string comparison of the product and the reversed string
+    return str(prod) == rev
+
+def reverseFinder(digits):    
+    for i in range(10**(digits-1),10**(digits)):
+        if reverseVerifier(i):
+            print(i)
+
+reverseFinder(10)
